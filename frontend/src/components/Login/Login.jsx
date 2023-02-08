3015c6db18f5334789b8e0dda4e300c7b0ae4659
ddhormuth0 <dawsonhormuth@gmail.com>
2023-02-07 21:04:22 -0500

    Change Cors

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import AuthService from "../services/auth_service"
-import NavBar from "../PageLayout/NavBar"
 
 function Login(props) {
 
